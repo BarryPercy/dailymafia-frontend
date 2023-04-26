dd3ba81b819e6f203dc0e82e1513a4287eb4de80
Barry Percy <baz.percy@gmail.com>
2023-04-26 17:12:02 +0100

    removing redundancies

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,7 +1,3 @@
-import { useState, useEffect } from "react";
-import { Col, Container, Row, Button } from "react-bootstrap";
-import { User } from "../../redux/interfaces";
-import axios from "axios";
 const Home = () =>{
     
     return(
